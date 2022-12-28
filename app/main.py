df4f458d6f73094275d104e15c3eab8aa519bca0
SMMousaviSP <smmousavisp@gmail.com>
2022-12-29 00:58:07 +0100

    Add create_user endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 
-from models import db
+from models import db, User
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
@@ -10,6 +10,18 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/create_user", methods=["POST"])
+def create_user_endpoint():
+    username = request.json.get("username")
+    email = request.json.get("email")
+    password = request.json.get("password")
+
+    user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+
+    return {
+        "code": 200,
+        "data": {"user_id": user.id},
+        "errors": []
+    }
