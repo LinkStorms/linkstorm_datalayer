c90caf6515594c56eab8528561af2a122eaf5da5
SMMousaviSP <smmousavisp@gmail.com>
2022-12-29 00:22:46 +0100

    Add db initialization and user model

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
+from models import db
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def hello_world():
