0205e54425fbd158f1d164ca15d18a13fbffc329
SMMousaviSP <smmousavisp@gmail.com>
2022-12-29 16:05:11 +0100

    Add ShortUrl model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,18 @@ class User(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    short_urls = db.relationship("ShortUrl", backref="user")
+
+    def __repr__(self):
+        return f"<User {self.id}: {self.username}>"
+
+
+class ShortUrl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    short_url = db.Column(db.String, unique=True, nullable=False)
+    long_url = db.Column(db.String, nullable=False)
+    note = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return f"<ShortUrl {self.id}: {self.short_url} -> {self.long_url}>"
